6150f570e32cfd44e2a6f43be9b11cc778513bff
Nguyen Hoang Phuc <phucnguyenhoang3839@gmail.com>
2024-12-16 10:56:12 +0700

    7_ReactingToEvent_React

--- example_format/src/components/TabButton.jsx
+++ example_format/src/components/TabButton.jsx
@@ -1,7 +1,13 @@
 export default function TabButton({ children }) {
+  function handleClick() {
+    console.log("hello  world!");
+  }
+  // you much not add a () into the pass prop function   
+  // if you add () the code inside it will execute when the code is executed  
   return (
     <li>
-      <button>{children}</button>
+     
+      <button onClick={handleClick}>{children}</button>
     </li>
   );
 }
