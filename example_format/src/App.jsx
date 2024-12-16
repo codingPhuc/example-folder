7eecea68e515c5b2649d178f6822948ce57d769c
Nguyen Hoang Phuc <phucnguyenhoang3839@gmail.com>
2024-12-16 10:26:13 +0700

    6_SpecialChirentProp_React

--- example_format/src/App.jsx
+++ example_format/src/App.jsx
@@ -1,16 +1,47 @@
+import Header from "./components/Header";
+import CoreConcept from "./components/Coreconcept";
+import reactCoreconcepts from "./assets/react-core-concepts.png";
+import config from "./assets/config.png";
+import jsxUi from "./assets/jsx-ui.png";
+import TabButton from "./components/TabButton";
+
+const listCoreConcepts = [
+  {
+    title: "Concept1",
+    description: "Description of Concept1",
+    imagePath: reactCoreconcepts,
+  },
+  {
+    title: "Concept2",
+    description: "Description of Concept2",
+    imagePath: config,
+  },
+  {
+    title: "Concept3",
+    description: "Description of Concept3",
+    imagePath: jsxUi,
+  },
+];
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header></Header>
       <main>
-        <h2>Time to get started!</h2>
+        {/* <h2>Time to get started!</h2> */}
+        <section id="core-concepts">
+          <ul>
+            <CoreConcept {...listCoreConcepts[0]}></CoreConcept>
+            <CoreConcept {...listCoreConcepts[1]}></CoreConcept>
+            <CoreConcept {...listCoreConcepts[2]}></CoreConcept>
+            {/* <CoreConcept></CoreConcept> */}
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Componets</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
